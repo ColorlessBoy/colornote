7f4491fb8f36755fec21ab90deafdf6d7f449cf5
penglingwei <penglingwei@zju.edu.cn>
2022-10-16 21:16:39 +0800

    add note: 热力学

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   semi: false,
   singleQuote: true,
-  printWidth: 100,
+  printWidth: 56,
   tabWidth: 2,
   useTabs: false,
   trailingComma: 'es5',
   bracketSpacing: true,
+  proseWrap: 'always',
 }
