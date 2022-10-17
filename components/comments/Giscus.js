c24a72d7079d819d067628eb5022790c2daa8b53
penglingwei <penglingwei@zju.edu.cn>
2022-10-17 17:12:15 +0800

    enable giscus comment system

--- components/comments/Giscus.js
+++ components/comments/Giscus.js
@@ -30,8 +30,6 @@ const Giscus = () => {
       lang,
     } = siteMetadata?.comment?.giscusConfig
 
-    console.log('giscus-repo', repo)
-
     const script = document.createElement('script')
     script.src = 'https://giscus.app/client.js'
     script.setAttribute('data-repo', repo)
